9620000a91d4b620fd2567ac003e5de518b59906
cty288 <cty288@163.com>
2022-02-25 15:43:56 -0500

    Shader, Cinemachine, DoTween

--- Assets/02. Scripts/Game/ViewController/GameCamera.cs
+++ Assets/02. Scripts/Game/ViewController/GameCamera.cs
@@ -15,11 +15,11 @@ namespace Mikrocosmos
         [SerializeField] private float lerp = 0.1f;
 
 
-
+        
         private void FixedUpdate() {
             
             if (following) {
-                transform.position = Vector3.Slerp(transform.position, new Vector3( following.transform.position.x, following.transform.position.y, -10), lerp * Time.deltaTime);
+                transform.position = Vector3.Slerp(transform.position, new Vector3( following.transform.position.x, following.transform.position.y, -10), lerp );
             }
         }
     }
