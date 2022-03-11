e6e88ef011e5a8b0799bd47869c97a56a213c098
cty288 <cty288@163.com>
2022-03-10 23:59:30 -0500

    Client Authority

--- Assets/02. Scripts/Game/ViewController/Player/PlayerSpaceship.Client.cs
+++ Assets/02. Scripts/Game/ViewController/Player/PlayerSpaceship.Client.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MikroFramework.Architecture;
 using UnityEngine;
 
 namespace Mikrocosmos
@@ -8,7 +9,12 @@ namespace Mikrocosmos
     public partial class PlayerSpaceship  : AbstractNetworkedController<Mikrocosmos> {
         [SerializeField]
         private bool isControlling = false;
+        private Rigidbody2D rigidbody;
 
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
         private void Update() {
             if (hasAuthority && isClient) {
                 RaycastHit2D ray = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition));
@@ -25,18 +31,27 @@ namespace Mikrocosmos
 
         private void FixedUpdate() {
             if (hasAuthority && isClient) {
-                Debug.Log("Hasauthority");
+                //Debug.Log("Hasauthority");
                 if (isControlling) {
-                    CmdAddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized);
+                    //CmdAddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized);
+                    Vector2 forceDir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position)
+                        .normalized;
+                    if (rigidbody.velocity.sqrMagnitude <= Mathf.Pow(this.GetModel<ISpaceshipConfigurationModel>().MaxSpeed, 2))
+                    {
+                        rigidbody.AddForce(forceDir * this.GetModel<ISpaceshipConfigurationModel>().MoveForce);
+                    }
                 }
 
-                CmdOnUpdateRotation(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                UpdateRotation(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             }
         }
 
-        public override void OnStartAuthority() {
-            base.OnStartAuthority();
-           // Camera.main.GetComponent<GameCamera>().following = this.gameObject;
+        private void UpdateRotation(Vector2 mousePos)
+        {
+            Vector2 dir = new Vector2(transform.position.x, transform.position.y) - mousePos;
+            float angle = Mathf.Atan2(dir.y, dir.x) * (180 / Mathf.PI) + 90;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), 0.2f);
         }
+
     }
 }
--- Assets/02. Scripts/Game/ViewController/Player/PlayerSpaceship.cs
+++ Assets/02. Scripts/Game/ViewController/Player/PlayerSpaceship.cs
@@ -8,29 +8,9 @@ using UnityEngine;
 namespace Mikrocosmos
 {
     public partial class PlayerSpaceship : AbstractNetworkedController<Mikrocosmos> {
-        private Rigidbody2D rigidbody;
-
-        private void Awake() {
-            rigidbody = GetComponent<Rigidbody2D>();
-        }
+        
 
 
-
-        [Command]
-        private void CmdAddForce(Vector2 forceDirection) {
-            if (rigidbody.velocity.sqrMagnitude <= Mathf.Pow(this.GetModel<ISpaceshipConfigurationModel>().MaxSpeed, 2)) {
-                rigidbody.AddForce(forceDirection * this.GetModel<ISpaceshipConfigurationModel>().MoveForce);
-            }
-
-           
-        }
-
-        [Command]
-        private void CmdOnUpdateRotation(Vector2 mousePos) {
-            Vector2 dir = new Vector2(transform.position.x, transform.position.y) - mousePos;
-            float angle = Mathf.Atan2(dir.y, dir.x) * (180 / Mathf.PI) + 90;
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), 0.2f );
-        }
        
     }
 }
