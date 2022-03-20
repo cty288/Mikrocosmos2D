08694f741e4d22e171d16c6e30d61d0b178c1437
Tim Chen <40655433+cty288@users.noreply.github.com>
2022-03-20 00:36:02 -0400

    可拓展视野的物体可开/关和只对指定玩家生效; 接口管理

---
+++ Assets/02. Scripts/Game/ViewController/Visions/CanCreateVisionViewController.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace Mikrocosmos
+{
+    public interface ICanCreateVisionViewController {
+        bool IsOn { get; set; }
+
+        void ServerTurnOn();
+        void ServerTurnOff();
+        void ServerAllowClientToSee(NetworkIdentity identity);
+
+        void ServerRemoveClient(NetworkIdentity identity);
+
+    }
+    public abstract class CanCreateVisionViewController : AbstractNetworkedController<Mikrocosmos>, ICanCreateVisionViewController {
+
+        #region Server
+
+        [field: SerializeField, SyncVar(hook = nameof(OnTurnOnStateChanged))]
+        public bool IsOn { get; set; } = false;
+
+        [SerializeField]
+        protected List<NetworkIdentity> clientCanSee = new List<NetworkIdentity>();
+
+        private Light2DBase visionLight;
+
+        protected virtual void Awake() {
+            visionLight = GetComponentInChildren<Light2DBase>();
+            visionLight.enabled = true;
+        }
+
+        public override void OnStartServer() {
+            base.OnStartServer();
+            visionLight.enabled = IsOn;
+        }
+
+
+        public void ServerTurnOn() {
+            IsOn = true;
+        }
+
+        public void ServerTurnOff() {
+            IsOn = false;
+        }
+
+        [ServerCallback]
+        public void ServerAllowClientToSee(NetworkIdentity identity) {
+            clientCanSee.Add(identity);
+            TargetOpenVision(identity.connectionToClient);
+        }
+
+
+        [ServerCallback]
+        public void ServerRemoveClient(NetworkIdentity identity) {
+            clientCanSee.Remove(identity);
+            TargetCloseVision(identity.connectionToClient);
+        }
+
+      
+
+
+        #endregion
+
+
+        #region Client
+        private bool isClientCanSee = false;
+
+        public override void OnStartClient() {
+            base.OnStartClient();
+            visionLight.enabled = IsOn;
+        }
+
+
+        [TargetRpc]
+        private void TargetOpenVision(NetworkConnection connection)
+        {
+            isClientCanSee = true;
+            ClientUpdateLight();
+        }
+        [TargetRpc]
+        private void TargetCloseVision(NetworkConnection connection)
+        {
+            isClientCanSee = false;
+            ClientUpdateLight();
+        }
+
+
+        [ClientCallback]
+        protected void ClientUpdateLight()
+        {
+            if (!isClientCanSee)
+            {
+                if (visionLight.enabled)
+                {
+                    visionLight.enabled = false;
+                    OnClientVisionLightTurnOff();
+                }
+                return;
+            }
+
+            visionLight.enabled = IsOn;
+        }
+
+        public void OnTurnOnStateChanged(bool lastIsTurnOn, bool currentIsTurnOn)
+        {
+            if (!isClientCanSee) return;
+            ClientUpdateLight();
+            if (currentIsTurnOn)
+            {
+                OnClientVisionLightTurnOn();
+            }
+            else
+            {
+                OnClientVisionLightTurnOff();
+            }
+        }
+
+      //  [ClientCallback]
+        protected abstract void OnClientVisionLightTurnOff();
+
+       // [ClientCallback]
+        protected abstract void OnClientVisionLightTurnOn();
+
+
+        #endregion
+
+    }
+}
