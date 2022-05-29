3c063cec3830cb651f7fd6621df50fa9254b8e65
Tim Chen <40655433+cty288@users.noreply.github.com>
2022-05-28 22:05:10 -0400

    太阳弹射碰撞他的物体

--- Assets/02. Scripts/Game/ViewController/Visions/Star.cs
+++ Assets/02. Scripts/Game/ViewController/Visions/Star.cs
@@ -1,17 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using MikroFramework.Utilities;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Mikrocosmos
 {
-    public class Star : CanCreateVisionViewController
-    {
+    public class Star : CanCreateVisionViewController {
+        [SerializeField] private float bounceForce;
+        [SerializeField] private LayerMask bounceAffectedLayers;
         protected override void OnClientVisionLightTurnOff() {
             
         }
 
         protected override void OnClientVisionLightTurnOn() {
             
+        }
+
+        private void OnCollisionEnter2D(Collision2D col) {
+            if (PhysicsUtility.IsInLayerMask(col.collider.gameObject, bounceAffectedLayers)) {
+                if (col.collider.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody)) {
+                    if (rigidbody.TryGetComponent<IHaveMomentum>(out IHaveMomentum model)) {
+                        Vector2 direction = rigidbody.transform.position - transform.position;
+                        //get the perpendicular vector of direction
+                        Vector2 ppd = Vector2.Perpendicular(direction);
+                        ppd *= Random.Range(0, 2) == 1 ? 1 : -1;
+                        direction += ppd;
+                        direction = direction.normalized;
+                        rigidbody.AddForce(direction * (rigidbody.mass + rigidbody.velocity.magnitude) * bounceForce, ForceMode2D.Impulse);
+                    }
+                    
+                }
+            }
         }
     }
 }
