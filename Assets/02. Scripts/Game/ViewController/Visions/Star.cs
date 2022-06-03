f1c4bf8bc75879b2622b1ef47f1f1152e31f532f
Tim Chen <40655433+cty288@users.noreply.github.com>
2022-06-02 21:57:46 -0400

    060202

--- Assets/02. Scripts/Game/ViewController/Visions/Star.cs
+++ Assets/02. Scripts/Game/ViewController/Visions/Star.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using MikroFramework.Utilities;
+using Mirror;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -21,15 +22,26 @@ namespace Mikrocosmos
         private void OnCollisionEnter2D(Collision2D col) {
             if (PhysicsUtility.IsInLayerMask(col.collider.gameObject, bounceAffectedLayers)) {
                 if (col.collider.TryGetComponent<Rigidbody2D>(out Rigidbody2D rigidbody)) {
-                    if (rigidbody.TryGetComponent<IHaveMomentum>(out IHaveMomentum model)) {
-                        Vector2 direction = rigidbody.transform.position - transform.position;
-                        //get the perpendicular vector of direction
-                        Vector2 ppd = Vector2.Perpendicular(direction);
-                        ppd *= Random.Range(0, 2) == 1 ? 1 : -1;
-                        direction += ppd;
-                        direction = direction.normalized;
-                        rigidbody.AddForce(direction * (rigidbody.mass + rigidbody.velocity.magnitude) * bounceForce, ForceMode2D.Impulse);
+
+                    if (rigidbody.TryGetComponent<IGoods>(out IGoods goods)) {
+                        if (goods.HookState == HookState.Freed) {
+                            NetworkServer.Destroy(rigidbody.gameObject);
+                        }
                     }
+                    else {
+                        if (rigidbody.TryGetComponent<IHaveMomentum>(out IHaveMomentum model))
+                        {
+                            Vector2 direction = rigidbody.transform.position - transform.position;
+                            //get the perpendicular vector of direction
+                            Vector2 ppd = Vector2.Perpendicular(direction);
+                            ppd *= Random.Range(0, 2) == 1 ? 1 : -1;
+                            direction += ppd;
+                            direction = direction.normalized;
+                            rigidbody.AddForce(direction * (rigidbody.mass + rigidbody.velocity.magnitude) * bounceForce, ForceMode2D.Impulse);
+                        }
+                    }
+
+                    
                     
                 }
             }
